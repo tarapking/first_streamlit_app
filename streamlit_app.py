602bd859b64e992802a7f6ea85670354cd1bebf2
tarapking <133238572+tarapking@users.noreply.github.com>
2023-05-17 08:36:56 +0000

    Update streamlit_app.py
    fruity vice responses added

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+# New section to show fruity vice streamlit response 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
