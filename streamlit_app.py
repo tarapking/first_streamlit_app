71e2a379c02c5356a6d2128380e6d1d2820192b2
tarapking <133238572+tarapking@users.noreply.github.com>
2023-05-17 11:12:28 +0000

    Update streamlit_app.py
    formatting and adding error messags

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,16 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to show fruity vice streamlit response 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-#streamlit.text(fruityvice_response.json()) #just writes the data to the screen 
-
-# take json version of the response and normalize it  
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output as a table
-streamlit.dataframe(fruityvice_normalized)
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+  else 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #trouble shoot
 streamlit.stop()
