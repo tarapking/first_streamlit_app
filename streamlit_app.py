a1e1aade76a74c8bc8400bbb8874d9e1bde4bc5d
tarapking <133238572+tarapking@users.noreply.github.com>
2023-05-17 08:58:45 +0000

    Update streamlit_app.py
    user input of fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected] 
@@ -25,9 +27,12 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to show fruity vice streamlit response 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/ fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen 
 
 # take json version of the response and normalize it  
